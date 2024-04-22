b1d32416f9454a79e52d7f294dae6675464f2380
Aman Kumar <amankr.02ak@gmail.com>
2024-04-22 22:58:28 +0530

    Setting up local Server dir

---
+++ src/Server/Server_instance/Routes/Index.js
@@ -0,0 +1,22 @@
+// just having visiual routes as this server is not running on any separate instance
+
+class LogIn {
+
+}
+
+class LogOut {
+    
+}
+
+class SignUp {
+    
+}
+
+class Active_Courses {
+
+}
+
+class Enrolled_Courses {
+    
+}
+
